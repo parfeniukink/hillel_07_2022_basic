cd8bb240523e8b99128be3ebcd4d107acb2f31b3
Dmytro Parfeniuk <parfeniukink@gmail.com>
2022-07-12 22:28:06 +0300

    Upload classwork files

--- lesson_5/1.py
+++ lesson_5/1.py
@@ -1,1 +1,13 @@
-print("Hello world")
+name = "John Doe"
+data = ["John", "Doe"]
+
+# for char in data:
+#     print(char)
+
+
+flag = True
+index = 0
+while flag:
+    element = data[index]
+    print(element)
+    index += 1
